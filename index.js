b97804fb7dae04e25d1895d045e9a2d6185e4f82
Sideborne <Sideborne@bk.ru>
2021-09-16 00:54:31 +0300

    add normalize and modified js

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
+class Basket {
+    constructor(garbage = '.basket') {
+        this.garbage = garbage;
+        this.garbageObj = [];
+        this.garbagePrice = 0;
+    }
 
-const renderGoodsItem = (title, price, img = '') => {
-    return `<div class="goods-item"><img src=${img} alt='some img'><h3>${title}</h3><p>${price}</p></div>`;
-};
+    _countBasketPrice() {
+        for (i = 0; i < garbageObj.length; i++) {
+            garbagePrice = garbagePrice + garbageObj[i].price;
+        }
+        return garbagePrice;
+    }
 
-const renderGoodsList = list => {
-    document.querySelector('.goods-list').insertAdjacentHTML('beforeend', list.map(item => renderGoodsItem(item.title, item.price)).join(''));
-};
+}
 
-renderGoodsList(goods);
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this._goods = [];
+        this._goodsObjects = [];
+
+        this._fetchGoods();
+        this._render();
+        this._countBasketPrice();
+    }
+
+    _fetchGoods() {
+        this._goods = [
+            { id: 1, title: 'Notebook', price: 20000 },
+            { id: 2, title: 'Mouse', price: 1500 },
+            { id: 3, title: 'Keyboard', price: 5000 },
+            { id: 4, title: 'Gamepad', price: 4500 },
+        ];
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (const product of this._goods) {
+            const productObject = new ProductItem(product);
+            this._goodsObjects.push(productObject);
+
+            block.insertAdjacentHTML('beforeend', productObject.getHTMLString())
+        }
+    }
+}
+
+class ProductItem {
+    constructor(item, img = 'https://via.placeholder.com/200x150') {
+        this.id = item.id;
+        this.title = item.title;
+        this.price = item.price;
+        this.img = img;
+    }
+
+    getHTMLString() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`;
+    }
+}
+
+const catalog = new ProductsList();
